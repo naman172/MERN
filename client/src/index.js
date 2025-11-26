2c433731118ddd5bf9cd1e2900b19584e26d89c6
Naman Bansal <namanbansal@Namans-MacBook-Air.local>
2025-11-26 07:25:19 +0530

    routing update

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
-import {Router} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import store from './store/index.js';
 import * as serviceWorker from './serviceWorker';
 
@@ -13,13 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <BrowserRouter>
         <App />
-      </Router>
+      </BrowserRouter>
     </Provider>
-  </React.StrictMode>);
+  </React.StrictMode>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
